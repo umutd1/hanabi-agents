added average of best and fixed the order of the games

examples/evolution_session.py
@@ -11,9 +11,11 @@ from evolve import Evolution
 import timeit
 import statistics
 import dill as pickle
+import matplotlib.pyplot as plt
+import plotter 
 
 ## Target for the pickle file
-target = 'Experiments/agent_checkpoint}_populatation_20.pickle'
+target = 'Experiments/populatation_20.pickle'
 
 load_pickled = False
 
@@ -21,16 +23,18 @@ start_time = timeit.default_timer()
 
 n_players = 2
 population_size = 20
-n_generations = 100     
-n_rules = 20      # Each agent is a list of these many rules
-elite_count = int(0.2 * population_size)            # We take 20% of the agents with the best score and retain them 
+n_generations = 10     
+n_rules = 10      # Each agent is a list of these many rules
+elite_count = int(0.25 * population_size)            # We take 20% of the agents with the best score and retain them 
+top_count = int(population_size * (0.2)) #evaluation of percentage of scores for evolution
+games_played = 5 #games played per agent
 
 '''
     Total number of games to be played -> Each agent plays 10 games against every other agent
     So, Population * population is one game for each against every other agent and then we multiply 
     it by 10 make it 10 games per agent against every other agent
 '''
-n_parallel = population_size * 10
+n_parallel = population_size * games_played
 
 # Initialize the environment
 env_conf = make_hanabi_env_config('Hanabi-Full', n_players)
@@ -69,20 +73,31 @@ for i in range(n_generations):
 
     #agent plays against all the other agents n_parallel times / population_size
     for k in range(population_size):
-        agents = [ParallelRulebasedAgent([my_rules[k]]), ParallelRulebasedAgent(my_rules)]
+        agents = [ParallelRulebasedAgent([my_rules[k]],n_parallel), ParallelRulebasedAgent(my_rules,n_parallel)]
         parallel_session = hmf.HanabiParallelSession(env, agents)
         #print("Game config", env.game_config)
         result = parallel_session.run_eval(dest = None, print_intermediate = False)
         #print(result)
+        result_average = np.average(np.reshape(result, (-1, games_played )), axis=1)
+        #print(result)
         #print("Average: ", result.mean())
-        scores.append(result.mean())
-
-    print("Gen:", i+1 , "Max:", max(scores), "Avg:", statistics.mean(scores) , "List:", scores)
-    evolution_config = Evolution(my_rules, scores, elite_count)
+        scores.append(result_average)
     
+    #discard the the unwanted percentage of scores, this is used for evolution
+    scores_sorted = np.sort(scores)[...,::-1]
+    scores_sorted = scores_sorted[...,:top_count]
+    scores_sorted = list(np.average(scores_sorted, axis = 1))
+
+
+    print("Gen:", i+1 , "Max:", max(scores_sorted), "Avg_best:", statistics.mean(scores_sorted) , "List:", scores)
+    evolution_config = Evolution(my_rules, scores_sorted, elite_count)
+    
+    target_file = "Experiments/Heatmaps/heatmap_gen_" + str(i+1)
+    plotter.plot(scores, target_file)
+
     my_rules = evolution_config.evolve()
     
-    best_agent = my_rules[scores.index(max(scores))]
+    best_agent = my_rules[scores_sorted.index(max(scores_sorted))]
     
     # Log the metrics and pickle the current data for checkpointing
     experiment_data['current_gen'] = i + 1